ca30903d1a6bf39de62c79f286745206bf7fa6ef
Mauro Sérgio da Costa Leal <mauroscl@gmail.com>
2013-01-15 18:05:49 -0200

    final do dia 15/01/2013

--- BsBios.Portal.Infra.Services/Implementations/AuthenticationProvider.cs
+++ BsBios.Portal.Infra.Services/Implementations/AuthenticationProvider.cs
@@ -1,4 +1,5 @@
-﻿using BsBios.Portal.Domain.Model;
+﻿using System.Web.Security;
+using BsBios.Portal.Domain.Model;
 using BsBios.Portal.Infra.Services.Contracts;
 
 namespace BsBios.Portal.Infra.Services.Implementations
@@ -8,16 +9,25 @@ namespace BsBios.Portal.Infra.Services.Implementations
         public UsuarioConectado Autenticar(string usuario, string senha)
         {
             usuario = usuario.ToLower();
+            UsuarioConectado usuarioConectado;
             if (usuario == "comprador" && senha == "123")
             {
-                return new UsuarioConectado(usuario,new PerfilComprador());
+                usuarioConectado = new UsuarioConectado(usuario,new PerfilComprador());
             }
             if (usuario == "fornecedor" && senha == "123")
             {
-                return new UsuarioConectado(usuario, new PerfilFornecedor());
+                usuarioConectado = new UsuarioConectado(usuario, new PerfilFornecedor());
             }
-
-            return new UsuarioConectado(usuario,new PerfilNaoAutorizado());
+            else
+            {
+                usuarioConectado = new UsuarioConectado(usuario,new PerfilNaoAutorizado());
+            }
+            if (usuarioConectado.Perfil.PermiteLogin)
+            {
+                FormsAuthentication.SetAuthCookie(usuario, true);    
+            }
+            
+            
         }
     }
 }
