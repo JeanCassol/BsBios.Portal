696f524df465d1dc4b06f2ec731ef0c0b82f029e
Mauro Leal <mauroscl@gmail.com>
2013-02-01 16:05:30 -0200

    Helpers

---
+++ BsBios.Portal.UI/Helpers/Coluna.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace BsBios.Portal.UI.Helpers
+{
+    public abstract class Coluna<TModel, TValue>
+    {
+        protected readonly Expression<Func<TModel, TValue>> Expressao;
+        protected readonly HtmlHelper<TModel> HtmlHelper;
+        protected readonly string InputClass;
+
+        protected Coluna(Expression<Func<TModel, TValue>> expressao, HtmlHelper<TModel> htmlHelper, string inputClass)
+        {
+            Expressao = expressao;
+            HtmlHelper = htmlHelper;
+            InputClass = inputClass;
+        }
+
+        public  MvcHtmlString GeraLabel()
+        {
+            return System.Web.Mvc.Html.LabelExtensions.LabelFor(HtmlHelper, Expressao);
+        }
+
+        public MvcHtmlString GeraMensagemDeValidacao()
+        {
+            return System.Web.Mvc.Html.ValidationExtensions.ValidationMessageFor(HtmlHelper, Expressao);
+        }
+
+        public abstract MvcHtmlString GeraInput();
+    }
+    public class ColunaComEditor<TModel, TValue> : Coluna<TModel, TValue>
+    {
+        public ColunaComEditor(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expressao)
+            : base(expressao, htmlHelper, "")
+        {
+        }
+
+        public override MvcHtmlString GeraInput()
+        {
+            return System.Web.Mvc.Html.EditorExtensions.EditorFor(HtmlHelper, Expressao);
+        }
+    }
+
+    public class ColunaComTextBox<TModel, TValue> : Coluna<TModel, TValue>
+    {
+        public ColunaComTextBox(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expressao, string inputClass)
+            : base(expressao, htmlHelper, inputClass)
+        {
+        }
+
+        public override MvcHtmlString GeraInput()
+        {
+            return System.Web.Mvc.Html.InputExtensions.TextBoxFor(HtmlHelper, Expressao, new { @class = InputClass });
+        }
+    }
+
+    public class ColunaComTextArea<TModel, TValue> : Coluna<TModel, TValue>
+    {
+        public ColunaComTextArea(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expressao)
+            : base(expressao, htmlHelper, "")
+        {
+        }
+
+        public override MvcHtmlString GeraInput()
+        {
+            return System.Web.Mvc.Html.TextAreaExtensions.TextAreaFor(HtmlHelper, Expressao, new { @rows = "5" });
+        }
+    }
+    public class ColunaComDropDown<TModel, TValue> : Coluna<TModel, TValue>
+    {
+        public ColunaComDropDown(HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expressao,
+            IEnumerable<SelectListItem> items, string nome, string inputClass = "")
+            : base(expressao, htmlHelper, inputClass)
+        {
+            _items = items;
+            _nome = nome;
+        }
+
+        private readonly IEnumerable<SelectListItem> _items;
+        private readonly string _nome;
+
+        public override MvcHtmlString GeraInput()
+        {
+            return System.Web.Mvc.Html.SelectExtensions.DropDownList(HtmlHelper, _nome, _items, "Selecione >>");
+        }
+    }
+}
--- BsBios.Portal.UI/Helpers/CustomHelpers.cs
+++ BsBios.Portal.UI/Helpers/CustomHelpers.cs
@@ -1,63 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
-using BsBios.Portal.ViewModel;
 
 namespace BsBios.Portal.UI.Helpers
 {
     public static class CustomHelpers
     {
 
-        private static string GeraColuna(Coluna coluna)
+        private static string GeraColuna<TModel, TValue>(Coluna<TModel, TValue> coluna)
         {
             return
                 "<div class=\"coluna\">" +
-                    @coluna.Label + 
-                    @coluna.Campo +
+                    @coluna.GeraLabel() +
+                    @coluna.GeraInput() +
                     "<p class=\"mensagemErro\">" +
-                        @coluna.MensagemDeValidacao +
+                        @coluna.GeraMensagemDeValidacao() +
                     "</p>" +
                 "</div>";
         }
 
-
-        public static IHtmlString LinhaComDuasColunas(this HtmlHelper html, Coluna coluna1, Coluna coluna2)
+        public static IHtmlString LinhaComDuasColunas<TModel, TValue>(this HtmlHelper html, Coluna<TModel, TValue> coluna1, Coluna<TModel, TValue> coluna2)
         {
             string retorno =
                 "<div class=\"linha\"> " +
                     GeraColuna(coluna1) +
                     GeraColuna(coluna2) +
                 "</div>";
-                return new HtmlString(retorno);
-        }
-
-        public static IHtmlString LinhaComDuasColunas<TModel, TValue>(this HtmlHelper<TModel> html, Coluna coluna1, Expression<Func<TModel, TValue>> expressao)
-        {
-            var coluna2 = new Coluna(System.Web.Mvc.Html.LabelExtensions.LabelFor(html, expressao),
-                                    System.Web.Mvc.Html.EditorExtensions.EditorFor(html, expressao),
-                                    System.Web.Mvc.Html.ValidationExtensions.ValidationMessageFor(html, expressao));
-
-            return LinhaComDuasColunas(html, coluna1, coluna2);
+            return new HtmlString(retorno);
         }
     }
 
-    public class Coluna//<T> where T:IViewModel
-    {
-        public MvcHtmlString Label { get; protected set; }
-        public MvcHtmlString Campo { get; protected set; }
-        public MvcHtmlString MensagemDeValidacao { get; protected set; }
 
-        public Coluna(MvcHtmlString label, MvcHtmlString campo, MvcHtmlString mensagemDeValidacao)
-        {
-            Label = label;
-            Campo = campo;
-            MensagemDeValidacao = mensagemDeValidacao;
-        }
-        //public Coluna (Func<T, TProperty> funcao )
-        //{
-        //}
-    }
 }
