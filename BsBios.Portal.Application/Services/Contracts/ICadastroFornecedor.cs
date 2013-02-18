426edb35c6c611455703a52e3ba87bc4f9c54a18
mauroscl <mauroscl@gmail.com>
2013-02-17 23:53:43 -0300

    cadastro de fornecedores

--- BsBios.Portal.Application/Services/Contracts/ICadastroFornecedor.cs
+++ BsBios.Portal.Application/Services/Contracts/ICadastroFornecedor.cs
@@ -1,9 +1,11 @@
-﻿using BsBios.Portal.ViewModel;
+﻿using System.Collections.Generic;
+using BsBios.Portal.ViewModel;
 
 namespace BsBios.Portal.Application.Services.Contracts
 {
     public interface ICadastroFornecedor
     {
         void Novo(FornecedorCadastroVm fornecedorCadastroVm);
+        void AtualizarFornecedores(IList<FornecedorCadastroVm> fornecedores);
     }
 }
