929f6ac73e47108f4fc87d8dae725104fcf93819
Mauro Leal <mauroscl@gmail.com>
2013-02-20 17:40:48 -0300

    View Model de requisição

---
+++ BsBios.Portal.ViewModel/RequisicaoDeCompraVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BsBios.Portal.ViewModel
+{
+    [DataContract]
+    public class RequisicaoDeCompraVm
+    {
+        public string Numero { get; set; }
+        public int NumeroItem { get; set; }
+        public string Descricao { get; set; }
+        public string Material { get; set; }
+        public decimal Quantidade { get; set; }
+        public string UnidadeMedida { get; set; }
+        public string Centro { get; set; }
+        public DateTime DataDeSolicitacao { get; set; }
+        public DateTime DataDeLiberacao { get; set; }
+        public DateTime DataDeRemessa { get; set; }
+        public string FornecedorPretendido { get; set; }
+        public string Requisitante { get; set; }
+        public string Criador { get; set; }
+    }
+}
