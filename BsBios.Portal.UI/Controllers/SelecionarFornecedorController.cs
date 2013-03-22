64aeb5c7b246135c5666e6699868897d45e3d134
mauroscl <mauroscl@gmail.com>
2013-03-22 08:04:56 -0300

    Cadastro de Quota

---
+++ BsBios.Portal.UI/Controllers/SelecionarFornecedorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using BsBios.Portal.UI.Filters;
+using BsBios.Portal.ViewModel;
+
+namespace BsBios.Portal.UI.Controllers
+{
+    [SecurityFilter]
+    public class SelecionarFornecedorController : Controller
+    {
+        public ActionResult Selecionar(FornecedorCadastroVm fornecedorCadastroVm)
+        {
+            return PartialView("_SelecionarFornecedor",fornecedorCadastroVm);
+        }
+
+    }
+}
