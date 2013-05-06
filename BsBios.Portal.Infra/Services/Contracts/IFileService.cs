ddb889fa1f39e8906eb5387ee33349bfdc4ab2e9
Mauro Leal <mauroscl@gmail.com>
2013-05-06 17:38:55 -0300

    Upload de arquivos

---
+++ BsBios.Portal.Infra/Services/Contracts/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace BsBios.Portal.Infra.Services.Contracts
+{
+    public interface IFileService
+    {
+        void Save(string basePath, string subFolder, string fileName, Stream fileStream);
+    }
+}
---
+++ BsBios.Portal.Infra/Services/Implementations/FileService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using BsBios.Portal.Common.Exceptions;
+using BsBios.Portal.Infra.Services.Contracts;
+
+namespace BsBios.Portal.Infra.Services.Implementations
+{
+    public class FileService : IFileService
+    {
+        public void Save(string basePath, string subFolder, string fileName, Stream fileStream)
+        {
+            string folderPath = Path.Combine(basePath, subFolder);
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            string filePath = Path.Combine(folderPath, fileName);
+            if (File.Exists(filePath))
+            {
+                throw new FileAlreadyExistsException(filePath);
+            }
+            var bytes = new Byte[fileStream.Length];
+            fileStream.Read(bytes, 0, (int) fileStream.Length);
+            File.WriteAllBytes(filePath, bytes);
+        }
+
+    }
+}
