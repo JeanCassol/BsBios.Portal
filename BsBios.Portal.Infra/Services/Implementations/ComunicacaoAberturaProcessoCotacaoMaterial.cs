21bf580965516420008d693362de5123f3115966
Mauro Leal <mauroscl@gmail.com>
2013-06-13 11:16:41 -0300

    Criação de regras de interface para tamanho dos campos; Alteração no método de comunicação da abertura do processo de cotação de material com o SAP;

--- BsBios.Portal.Infra/Services/Implementations/ComunicacaoAberturaProcessoCotacaoMaterial.cs
+++ BsBios.Portal.Infra/Services/Implementations/ComunicacaoAberturaProcessoCotacaoMaterial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Xml.Serialization;
@@ -13,8 +14,33 @@ namespace BsBios.Portal.Infra.Services.Implementations
 {
     public class ComunicacaoAberturaProcessoCotacaoMaterial : IProcessoDeCotacaoComunicacaoSap
     {
+        private readonly IComunicacaoSap<ProcessoDeCotacaoDeMaterialAberturaComunicacaoSapVm> _comunicacaoSap;
+
+        public ComunicacaoAberturaProcessoCotacaoMaterial(IComunicacaoSap<ProcessoDeCotacaoDeMaterialAberturaComunicacaoSapVm> comunicacaoSap)
+        {
+            _comunicacaoSap = comunicacaoSap;
+        }
+
         public ApiResponseMessage EfetuarComunicacao(ProcessoDeCotacao processo)
         {
+            foreach (var fornecedorParticipante in processo.FornecedoresParticipantes)
+            {
+                var vm = new ProcessoDeCotacaoDeMaterialAberturaComunicacaoSapVm
+                    {
+                        IdProcessoCotacao = processo.Id,
+                        CodigoFornecedor = fornecedorParticipante.Fornecedor.Codigo ,
+                        Itens = (ListaProcessoDeCotacaoDeMaterialAberturaItemVm) (from item in processo.Itens
+                                 let itemMaterial = (ProcessoDeCotacaoDeMaterialItem) item
+                                 select new ProcessoDeCotacaoDeMaterialAberturaItemVm
+                                     {
+                                         NumeroRequisicao = itemMaterial.RequisicaoDeCompra.Numero ,
+                                         NumeroItem = itemMaterial.RequisicaoDeCompra.NumeroItem
+                                     }).ToList()
+                    };
+
+                ApiResponseMessage resposta =  _comunicacaoSap.EnviarMensagem("", vm);
+
+            }
             return new ApiResponseMessage(){Retorno = new Retorno(){Codigo = "200" ,Texto = "Esta comunicação ainda não foi implementada." }};
         }
     }
