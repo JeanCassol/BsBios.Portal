bd7776e0a83f06170c26822b75d765008e7c554d
Mauro Leal <mauroscl@gmail.com>
2013-11-23 06:07:07 -0200

    comando para backup e restore no oracle

---
+++ DataBase/comandos para backup e restore.sql
@@ -0,0 +1,34 @@
+----COMANDOS PARA EXECUTAR NO SERVIDOR ONDE SERÁ FEITO BACKUP
+
+--comandos para executar com usuario sys no sqlplus
+grant create any directory to bsbios;
+grant EXP_FULL_DATABASE to bsbios;
+grant IMP_FULL_DATABASE to bsbios;
+
+--comandos para executar com o usuário que vai fazer o backup do seu esquema no sqlplus
+create or replace directory dmp_dir as 'c:\tmp';
+
+--comandos para executar no prompt de comando
+expdp bsbios/fusion123 schemas=bsbios directory=dmp_dir dumpfile=bsbios_20131119.dmp logfile=bsbios_20131119.log
+
+
+--=====================================================================
+--COMANDOS PARA EXECUTAR NO SERVIDOR ONDE SERÁ FEITO RESTORE
+
+--comandos para executar com usuário sys no sqlplus
+drop user bsbios CASCADE;
+create user bsbios identified by fusion123 
+DEFAULT TABLESPACE users 
+TEMPORARY TABLESPACE temp;
+grant resource, connect, create view, create table, create sequence, create any directory, unlimited tablespace, EXP_FULL_DATABASE, IMP_FULL_DATABASE to bsbios;
+
+--comandos para executar com o usuário que vai fazer o restore
+create or replace directory dmp_dir as 'c:\tmp';
+
+
+--comando para executar no prompt de comando
+impdp bsbios/fusion123 schemas=bsbios directory=dmp_dir dumpfile=bsbios_20131119.dmp logfile=bsbios_20131119.log
+
+
+
+
