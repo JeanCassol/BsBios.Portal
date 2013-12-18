e95528f07b2ca04e24c5220f8d7f15a749977d5a
mauroscl <mauro123>
2013-12-18 09:39:14 -0200

    correções nos relatórios de cotação e OT

--- BsBios.Portal.ViewModel/RelatorioDeOrdemDeTransporteAnaliticoVm.cs
+++ BsBios.Portal.ViewModel/RelatorioDeOrdemDeTransporteAnaliticoVm.cs
@@ -1,8 +1,11 @@
-﻿namespace BsBios.Portal.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BsBios.Portal.ViewModel
 {
     public class RelatorioDeOrdemDeTransporteAnaliticoVm : ProcessoDeCotacaoDeFreteBaseVm
     {
         public virtual string Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0}")]
         public virtual decimal IdDaOrdemDeTransporte { get; set; }
         public virtual string Transportadora { get; set; }
         public virtual decimal QuantidadeContratada { get; set; }
@@ -11,8 +14,10 @@
         public virtual decimal QuantidadeEmTransito { get; set; }
         public virtual decimal QuantidadeRealizada { get; set; }
         public virtual decimal QuantidadePendente { get; set; }
-        public virtual int QuantidadeDeColetasRealizadas { get; set; }
-        public virtual int QuantidadeDeDiasEmAtraso { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0}")]
+        public virtual decimal QuantidadeDeColetasRealizadas { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0}")]
+        public virtual decimal QuantidadeDeDiasEmAtraso { get; set; }
         public virtual decimal PercentualDeAtraso { get; set; }
 
     }
