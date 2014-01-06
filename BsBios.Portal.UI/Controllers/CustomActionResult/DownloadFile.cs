a2a62dc721910ee4d15721c041de65068730b96c
Mauro Leal <mauroscl@gmail.com>
2014-01-06 21:33:06 -0200

    exportação para excel

---
+++ BsBios.Portal.UI/Controllers/CustomActionResult/DownloadFile.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BsBios.Portal.UI.Controllers.CustomActionResult
+{
+    public class DownloadFileActionResult : ActionResult
+    {
+        public string ExcelGridView { get; set; }
+        public string FileName { get; set; }
+
+        public DownloadFileActionResult(string gridView, string fileName)
+        {
+            ExcelGridView = gridView;
+            FileName = fileName;
+
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+
+            //Create a response stream to create and write the Excel file
+            HttpContext curContext = HttpContext.Current;
+            curContext.Response.Clear();
+            curContext.Response.AddHeader("content-disposition", "attachment;filename=" + FileName);
+            curContext.Response.Charset = "";
+            curContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            curContext.Response.ContentType = "application/ms-excel";
+            //curContext.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            //Open a memory stream that you can use to write back to the response
+            byte[] byteArray = Encoding.ASCII.GetBytes(ExcelGridView);
+            MemoryStream s = new MemoryStream(byteArray);
+            StreamReader sr = new StreamReader(s, Encoding.Unicode);
+
+            //FileStream fileStream = new FileStream("c:\\tmp\\RelatorioSalvo.xls",FileMode.Create);
+            //fileStream.Write(byteArray, 0, byteArray.Length);
+
+            //Write the stream back to the response
+            curContext.Response.ContentEncoding = System.Text.Encoding.Unicode;
+            curContext.Response.Write(sr.ReadToEnd());
+
+            //fileStream.Close();
+            curContext.Response.End();
+
+        }
+    }
+}
--- BsBios.Portal.UI/Controllers/RelatorioDeProcessoDeCotacaoDeFreteVisualizacaoController.cs
+++ BsBios.Portal.UI/Controllers/RelatorioDeProcessoDeCotacaoDeFreteVisualizacaoController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Web.Mvc;
 using BsBios.Portal.Application.Queries.Contracts;
 using BsBios.Portal.Common;
+using BsBios.Portal.UI.Controllers.CustomActionResult;
 using BsBios.Portal.UI.Filters;
 using BsBios.Portal.ViewModel;
 using Microsoft.Ajax.Utilities;
@@ -46,6 +48,70 @@ namespace BsBios.Portal.UI.Controllers
             }
 
             
+        }
+
+        //public ActionResult GerarRelatorioEmExcel(RelatorioDeProcessoDeCotacaoDeFreteFiltroVm filtro)
+        //{
+
+        //    ViewBag.Filtro = filtro;
+
+        //    IList<RelatorioDeProcessoDeCotacaoDeFreteAnaliticoVm> processos = _consulta.ListagemAnalitica(filtro);
+        //    ViewBag.TituloDoRelatorio = "Relatório de Processo de Cotação de Frete - Analítico";
+        //    View("ListagemAnalitica", processos).ExecuteResult(ControllerContext);
+        //    Response.ContentType = "application/vnd.ms-excel";
+        //    Response.ContentEncoding = System.Text.Encoding.UTF8;
+        //    Response.HeaderEncoding = System.Text.Encoding.UTF8;
+        //    Response.AddHeader("content-disposition", "attachment; filename=Relatorio.xls");
+
+        //    //Stream outputStream = this.Response.OutputStream;
+
+        //    //long tamanhoDoConteudo = outputStream.Length;
+
+        //    //var dadosParaSalvar = new byte[tamanhoDoConteudo];
+
+        //    //outputStream.Read(dadosParaSalvar, 0,(int) tamanhoDoConteudo);
+
+        //    //var fileStream = new FileStream(".\\RelatorioSalvo.xls",FileMode.Create);
+
+        //    //fileStream.Write(dadosParaSalvar, 0, (int) tamanhoDoConteudo);
+
+        //    //fileStream.Close();
+
+        //    return null;
+        //}
+
+        public ActionResult GerarRelatorioEmExcel(RelatorioDeProcessoDeCotacaoDeFreteFiltroVm filtro)
+        {
+            ViewBag.Filtro = filtro;
+            ViewBag.TituloDoRelatorio = "Relatório de Processo de Cotação de Frete - Analítico";
+
+            IList<RelatorioDeProcessoDeCotacaoDeFreteAnaliticoVm> processos = _consulta.ListagemAnalitica(filtro);
+            string html = RenderPartialViewToString("ListagemAnalitica", processos);
+
+            html = html.Replace("\n", "");
+            html = html.Replace("\r", "");
+            html = html.Replace("  ", "");
+
+            var downloadFileActionResult = new DownloadFileActionResult(html, "RelatorioDeProcessoDeCotacaoAnalitico.xls");
+
+            return downloadFileActionResult;
+        }
+
+        protected string RenderPartialViewToString(string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+                viewName = ControllerContext.RouteData.GetRequiredString("action");
+
+            ViewData.Model = model;
+
+            using (StringWriter sw = new StringWriter())
+            {
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                return sw.GetStringBuilder().ToString();
+
+            }
         }
 
     }
